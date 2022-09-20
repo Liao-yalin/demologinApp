c95995795f0478b0f96ff0ac5c329406b501e6d4
Liao-yalin <cxghk581@gmail.com>
2022-09-20 16:24:04 +0800

    work

---
+++ app/src/main/java/com/example/a0913class/UserActivity.java
@@ -0,0 +1,50 @@
+package com.example.a0913class;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        Button dialbtn = findViewById(R.id.dialbtn);
+        dialbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri phone = Uri.parse("tel://123456");
+                Intent it = new Intent(Intent.ACTION_DIAL,phone);
+                startActivity(it);
+
+            }
+        });
+
+        Button webbtn = findViewById(R.id.webbtn);
+        webbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri web = Uri.parse("https://www.google.com");
+                Intent it = new Intent(Intent.ACTION_VIEW,web);
+                startActivity(it);
+            }
+        });
+
+        Button exitbtn = findViewById(R.id.exitbtn);
+        exitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Toast.makeText(getApplicationContext(), "登出成功",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
